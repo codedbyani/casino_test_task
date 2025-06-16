9183f254bb39106725d8c308621e79fcad775a04
codedbyani <antonyanani26@gmail.com>
2025-06-16 09:54:24 +0400

    Added SVGs for sidebar

---
+++ src/assets/svg/MenuHamburgerIcon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { IIconProps } from '@/types/ui/iconsType';
+
+export const MenuHamburgerIcon: React.FC<IIconProps> = (props) => {
+    return (
+        <svg
+            width='20'
+            height='16'
+            viewBox='0 0 20 16'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            {...props}
+        >
+            <path
+                d='M1 1H19M1 8H19M1 15H19'
+                stroke='currentColor'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+            />
+        </svg>
+    );
+};
